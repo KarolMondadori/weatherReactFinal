e50c35d4b46966bbc98a31569d1ac83a06a8cf85
Karol Mondadori <karolmondadori@gmail.com>
2021-05-01 18:03:02 -0300

    included forecast 5/6 days
    *useEffect*

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,14 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
+
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.data.coord]);
 
     function handleResponse(response) {
     setForecast(response.data.daily);
@@ -15,9 +19,14 @@ export default function WeatherForecast(props) {
         return (
             <div className="forecasthour">
                 <div className="row">
-                    <div className="col">
-                        <WeatherForecastDay data={forecast[0]}/>
-                    </div>
+                    {forecast.map(function (dailyForecast, index){
+                        if (index < 6) {
+                        return (
+                            <div className="col" key={index}>
+                            < WeatherForecastDay data={dailyForecast}/>
+                            </div> ); }
+                        })}
+                    
                 </div>
             </div>
             );
